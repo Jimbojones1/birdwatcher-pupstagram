1b572938f7cf78fc833f4e664e1d185c30bb6e1a
JimHaff <james.haff@generalassemb.ly>
2023-05-01 13:43:19 -0500

    added multer

--- controllers/users.js
+++ controllers/users.js
@@ -8,18 +8,21 @@ module.exports = {
 };
 
 async function signup(req, res) {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    const token = createJWT(user);
-    // the code that runs in response to this on the client
-    // is in the utils/userService signup function, that last
-    // .then! 
-    res.json({ token });
-  } catch (err) {
-    // Probably a duplicate email
-    res.status(400).json(err);
-  }
+
+  console.log(req.body, ' < req.body', req.file, " <- req.filed")
+  res.json({data: 'hitting signup!'})
+  // const user = new User(req.body);
+  // try {
+  //   await user.save();
+  //   const token = createJWT(user);
+  //   // the code that runs in response to this on the client
+  //   // is in the utils/userService signup function, that last
+  //   // .then! 
+  //   res.json({ token });
+  // } catch (err) {
+  //   // Probably a duplicate email
+  //   res.status(400).json(err);
+  // }
 }
 // https://git.generalassemb.ly/SEI-CC/sei-2-21-birdwatchers/blob/main/work/w11/d1/jwt-boilerplatec-code.md
 async function login(req, res) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const usersCtrl = require("../../controllers/users");
-
+const multer = require('multer');
+const upload = multer();
 /*---------- Public Routes ----------*/
-router.post("/signup",  usersCtrl.signup);
+
+// whichever route is handling a file/photo upload, you use multer
+// 'photo' comes from the key on the form-data object we created 
+// on the signup page in the react code!
+router.post("/signup", upload.single('photo'), usersCtrl.signup);
 router.post("/login", usersCtrl.login);
 
 /*---------- Protected Routes ----------*/
