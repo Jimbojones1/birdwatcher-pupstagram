e2fd3ed8198fb669f2c5ae14f8c48bdb811b34d7
JimHaff <james.haff@generalassemb.ly>
2023-05-01 14:36:23 -0500

    Added signup working

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,7 @@ module.exports = {
 
 const S3 = require('aws-sdk/clients/s3');
 // initialize the constructor function
+// this object can make requests to our s3 bucket!
 const s3 = new S3();
 
 // we'll use the module uuid to generate random names for our aws file
@@ -25,7 +26,37 @@ const BUCKET_NAME = process.env.BUCKET
 async function signup(req, res) {
 
   console.log(req.body, ' < req.body', req.file, " <- req.filed")
-  res.json({data: 'hitting signup!'})
+  
+  // first check to make sure the user uploaded a file
+  if(!req.file) return res.status(400).json({error: "Please Submit a Photo!"});
+
+  // create our path of where we want to strore file in our s3 bucket
+  const filePath = `pupstagram/${uuidv4()}-${req.file.originalname}`;
+  const params = {Bucket: BUCKET_NAME, Key: filePath, Body: req.file.buffer}; // req.file.buffer is the image uploaded from the client
+  // sd.upload to make a request to s3 bucket
+  s3.upload(params, async function(err, data){
+    if(err){
+      console.log('===========================================')
+      console.log(err, ' err from aws, either your bucket name is wrong or your keys arent correct');
+      console.log('===========================================')
+      res.status(400).json({error: 'Error from aws, check your terminal!'})
+    }
+
+    const user = new User({...req.body, photoUrl: data.Location}); // data.Location is the is the url of our image on AWS
+    try {
+      await user.save(); // user model .pre('save') will run which hashes our password
+      // create our jwt token
+      const token = createJWT(user);
+      res.json({token}); // sends the token to the client (userService.signup function) which stores to the token in localstorage!
+      // our user model toJSON removes the password
+
+    } catch(err){
+      res.status(400).json({error: err})
+    }
+
+  }) // end of s3.upload
+
+
   // const user = new User(req.body);
   // try {
   //   await user.save();
